1fa564e6aedb91ac818ccf7c9c373661fc58087d
Sebastian Boolean <truefalse@posteo.de>
2022-12-13 16:24:46 +0100

    Initial commit from Create Next App

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+
+export default function Home({ formattedDate }: any) {
+  return (
+    <>
+      <h1>Static page</h1>
+      <p>This page is static. It was built on {formattedDate}.</p>
+      <p>
+        <Link href="/ssr">View a server-side rendered page.</Link>
+      </p>
+    </>
+  );
+}
+export async function getStaticProps() {
+  const buildDate = Date.now();
+  const formattedDate = new Intl.DateTimeFormat('en-US', {
+    dateStyle: 'long',
+    timeStyle: 'long',
+  }).format(buildDate);
+
+  return { props: { formattedDate } };
+}
